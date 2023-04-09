7dc2dc3515216e39cc716e34462ea5a49f5375e4
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-04-10 01:57:37 +0600

    title added img

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Home.css"
 import Title from '../Title/Title';
+import JobCatagory from '../JobCatagory/JobCatagory';
 
 const Home = () => {
     return (
         <div>
            <Title></Title>
+           
+           <JobCatagory></JobCatagory>
 
         </div>
     );
---
+++ src/Components/JobCatagory/JobCatagory.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./JobCatagory.css"
+
+const JobCatagory = () => {
+    return (
+        <div>
+            <div className='jobCtg'>
+            <h2>Job Category List</h2>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+            </div>
+          
+        </div>
+    );
+};
+
+export default JobCatagory;
